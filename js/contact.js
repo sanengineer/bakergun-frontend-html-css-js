74cfe48e385107fdfed19ab0f123070809921bb4
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-09-20 15:15:17 +0700

    add bg contact page

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+const req = new XMLHttpRequest();
+const contFeatImg = document.getElementById("sectionNewLettFeatureImage");
+const contactPageBgBottom = document.getElementById("contactPageFootBgSrc");
+const apiv1 = "http://localhost:8008/api/v1";
+
+req.open("GET", `${apiv1}/images`, true);
+req.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  // Checking Respon Data
+  console.log(data);
+
+  if (req.status >= 200 && req.status < 400) {
+    // Checking Respon Data
+    console.log(`file image url: ${data[34].imageUrl}`);
+    console.log(`file image url: ${data[35].imageUrl}`);
+
+    contactPageBgBottom.src = data[34].imageUrl;
+    contFeatImg.src = data[35].imageUrl;
+  } else {
+    console.log("nothing respond");
+  }
+};
+
+req.send(); // ini buat apa ya?? :P
